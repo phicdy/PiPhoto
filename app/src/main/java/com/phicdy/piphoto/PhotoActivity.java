c3aeb959a7a357ce941fee2d9c36612efc74d98a
phicdy <phicdy@gmail.com>
2016-02-01 23:29:24 +0900

    Add share button

--- app/src/main/java/com/phicdy/piphoto/PhotoActivity.java
+++ app/src/main/java/com/phicdy/piphoto/PhotoActivity.java
@@ -2,8 +2,11 @@ package com.phicdy.piphoto;
 
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 import com.phicdy.piphoto.util.FileUtil;
@@ -11,15 +14,40 @@ import com.phicdy.piphoto.util.FileUtil;
 
 public class PhotoActivity extends ActionBarActivity {
 
+    private String photoPath;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
 
         Intent intent = getIntent();
-        String photoPath = intent.getStringExtra(FileUtil.EXTRA_PHOTO);
-        ImageView imageView = (ImageView)findViewById(R.id.image);
+        photoPath = intent.getStringExtra(FileUtil.EXTRA_PHOTO);
+        ImageView imageView = (ImageView) findViewById(R.id.image);
         imageView.setImageBitmap(BitmapFactory.decodeFile(photoPath));
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_photo, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.share:
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_STREAM, Uri.parse(photoPath));
+                intent.setType("image/*");
+                startActivity(intent);
+                break;
+            default:
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
